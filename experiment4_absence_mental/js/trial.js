95c88f431b3cd4f056069761618574f4c4c62e86
Louise Li <129934489+CSLouise@users.noreply.github.com>
2026-02-01 23:47:10 -0800

    experiment 4 pilot absence hurt for physical and mental

---
+++ experiment4_absence_mental/js/trial.js
@@ -0,0 +1,20 @@
+const trials = {
+    "instruction": "images/instruction.png",
+    "story": [
+        "images/Slide24.png",
+        "images/Slide25.png",
+        "images/Slide26.png",
+        "images/Slide27.png",
+        "images/Slide28.png",
+        "images/Slide29.png",
+        "images/Slide30.png",
+        "images/Slide31.png",
+        "images/Slide32.png",
+        "images/Slide33.png",
+        "images/Slide34.png"
+    ],
+    "questions": {
+        "cause_hurt": "images/q_cause_hurt.png",
+        "hurt": "images/q_hurt.png"
+    }
+}
---
+++ experiment4_absence_physical/js/trial.js
@@ -0,0 +1,28 @@
+const trials = {
+    "instruction": "images/instruction.png",
+    "story": [
+        "images/Slide2.png",
+        "images/Slide3.png",
+        "images/Slide4.png",
+        "images/Slide5.png",
+        "images/Slide6.png",
+        "images/Slide7.png",
+        "images/Slide8.png",
+        "images/Slide9.png",
+        "images/Slide10.png",
+        "images/Slide11.png",
+        "images/Slide12.png",
+        "images/Slide13.png",
+        "images/Slide14.png",
+        "images/Slide15.png",
+        "images/Slide16.png",
+        "images/Slide17.png",
+        "images/Slide18.png",
+        "images/Slide19.png",
+        "images/Slide20.png"
+    ],
+    "questions": {
+        "cause_hurt": "images/q_cause_hurt.png",
+        "hurt": "images/q_hurt.png"
+    }
+}
