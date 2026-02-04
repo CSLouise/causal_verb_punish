b1b4474909b91d1533decab0938b6f88ec0b3751
Louise Li <129934489+CSLouise@users.noreply.github.com>
2026-02-03 22:34:29 -0800

    add experiment4 v2

---
+++ experiment4_absence_mental_v2/js/trial.js
@@ -0,0 +1,43 @@
+const trials = {
+    "instruction": "images/instructions.png",
+    "scenarios": {
+        "hurt": {
+            "story": [
+                "images/hurt_mental/Slide52.png",
+                "images/hurt_mental/Slide53.png",
+                "images/hurt_mental/Slide54.png",
+                "images/hurt_mental/Slide55.png",
+                "images/hurt_mental/Slide56.png",
+                "images/hurt_mental/Slide57.png",
+                "images/hurt_mental/Slide58.png",
+                "images/hurt_mental/Slide59.png",
+                "images/hurt_mental/Slide60.png",
+                "images/hurt_mental/Slide61.png",
+                "images/hurt_mental/Slide62.png"
+            ],
+            "questions": {
+                "cause": "images/hurt_mental/q_cause_hurt_mental.png",
+                "lexical": "images/hurt_mental/q_hurt_mental.png"
+            }
+        },
+        "shock": {
+            "story": [
+                "images/shock_mental/Slide66.png",
+                "images/shock_mental/Slide67.png",
+                "images/shock_mental/Slide68.png",
+                "images/shock_mental/Slide69.png",
+                "images/shock_mental/Slide70.png",
+                "images/shock_mental/Slide71.png",
+                "images/shock_mental/Slide72.png",
+                "images/shock_mental/Slide73.png",
+                "images/shock_mental/Slide74.png",
+                "images/shock_mental/Slide75.png",
+                "images/shock_mental/Slide76.png"
+            ],
+            "questions": {
+                "cause": "images/shock_mental/q_cause_shock_mental.png",
+                "lexical": "images/shock_mental/q_shock_mental.png"
+            }
+        }
+    }
+}
